fee7e42f2cb4959da6bf5ac3b6ea256ae93b496c
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-10 18:48:29 -0500

    Create FruitArray.js

---
+++ FruitArray.js
@@ -0,0 +1,20 @@
+//Array Indexing Practice
+
+//Remeber that an array starts at index 0
+
+//You have several fruits in a row on the shelf
+let fruits = ["apple","banana","pineapple"];
+
+//List the fruits in order
+
+console.log("The fruits on the shelf are (in order):");
+
+console.log("1st: "+fruits[0]);
+console.log("2nd: "+fruits[1]);
+console.log("3rd: "+fruits[2]);
+
+
+//Now we want to add a new fruit. Use the PUSH method
+
+fruits.push("cherry");
+console.log("4th: "+fruits[3]);
