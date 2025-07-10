abe830befdf7c2bfbfbbaa6f7f653f3b19a45878
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-10 18:57:15 -0500

    Update FruitArray.js

--- FruitArray.js
+++ FruitArray.js
@@ -23,3 +23,19 @@ console.log("\nAfter Adding Fruit\n4th: "+fruits[3]);
 //Now remove that last index using the pop method
 fruits.pop();//Will remove the newly added 'cherry' string in the last index
 console.log("\nAfter removing the last index (4th item)\n4th: "+fruits[3]+"\nThe cherry has been removed!");
+
+
+//How many fruits do you have now? 
+//use the 'length' property to find out
+
+let numFruits = fruits.length;
+console.log("\nThere are "+numFruits+" fruits!")
+
+//Add more fruits!
+fruits.push("watermelon","blueberry","dragon fruit");
+
+//Now how many?
+numFruits = fruits.length;//To store new values within the numFruits variable
+console.log("\nThere are "+numFruits+" fruits!")
+
+//use a for loop to print out all fruits
