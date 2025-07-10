3c26010f19cd433b94a0e8f4ebed312077571c5b
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-10 18:27:58 -0500

    Update BankAccountObjects.js

--- BankAccountObjects.js
+++ BankAccountObjects.js
@@ -8,20 +8,37 @@ function BankAccount(username,balance){
     this.deposit = function deposit(amount){
         this.balance += amount; // add to balance
         
-    }
+    }//End deposit
     
     this.withdraw = function withdraw(amount){
         
-        if (amount <= this.balance) {
-      this.balance -= amount; // subtract from balance
-    } else {
-      console.log("Insufficient funds");
-    }
+         if (amount <= this.balance) {
+          this.balance -= amount; // subtract from balance
+        } //end if
+        
+        else {
+          console.log("Insufficient funds");
+        }//end else
+        
+    }//End withdraw
     
-//You don’t need return in a constructor — the new keyword automatically returns the new object.
+    //function for displaying the name and balance
+    this.getNameBalance = function(){
+        
+        return "The username of this account is "+username+" \nThe balance of this account is "+balance;
+        
+    }//End this.getNameBalance
     
-}
+    
+        //NO RETURN
+        //You don’t need return in a constructor — the new keyword automatically returns the new object.
+    
+}//End function
+    
 
 var newUser1 = new BankAccount("jondoe57",2000);
+console.log(newUser1.getNameBalance());
 
+newUser1.deposit(500); // adds 500
+newUser1.withdraw(1000); // subtracts 1000
 // console.log(newUser1.BankAccount()); is not correct — you’re calling a method named BankAccount but you don’t have one. The constructor itself is not a method on the object — it created the object.
