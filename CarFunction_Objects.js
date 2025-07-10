3b02cc20ca48c198b96166eedc920fd5432fd0e1
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-10 16:09:54 -0500

    Update CarFunction_Objects.js

--- CarFunction_Objects.js
+++ CarFunction_Objects.js
@@ -1,9 +1,15 @@
 //Creating Custom Objects
 
 function Car(make,model,year){
-    //there are three argumnets in the function: make,model,and year
+    //there are three parameters in the function: make,model,and year
+    
+    //make,model,and year(parameters) are only temporary names for whats passed into the function
+    
+    //in order to add a property to the car object by that name you have to do : 
+        //this.parameterName = parameterName
     
     this.make = make;
+    //adds a property called 'make' to the Car object sets it to what ever is passed into the make parameter
     
     this.model = model;
     
@@ -13,6 +19,8 @@ function Car(make,model,year){
         
         return this.make + '' + this.model +'' + this.year ;
         
+        //the 'this keyword makes sure each Car object keeps its own values, as multiple car objects can be made from this constructor
+        
     }//end getName
     
     
@@ -20,3 +28,5 @@ function Car(make,model,year){
 
 var c = new Car ("Meridian", "Saber GT", "2012");
 alert(c.getName()); //displays "Meridian Saber GT 2012"
+
+//alert() is a built-in JavaScript function that pops up a small browser dialog box with a message.
