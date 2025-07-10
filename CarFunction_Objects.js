ffc9e9aa7eefc06749bf6cdb466f001e539795f9
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-10 16:40:34 -0500

    Update CarFunction_Objects.js

--- CarFunction_Objects.js
+++ CarFunction_Objects.js
@@ -10,14 +10,16 @@ function Car(make,model,year){
     
     this.make = make;
     //adds a property called 'make' to the Car object sets it to what ever is passed into the make parameter
+    //the this keyword is an indication that an object is being made
     
     this.model = model;
     
     this.year = year;
     
     this.getName = function(){
+        //getName is a method or function stored in the object
         
-        return this.make + '' + this.model +'' + this.year ;
+        return this.make + ' ' + this.model + ' ' + this.year + ' ';
         
         //the 'this keyword makes sure each Car object keeps its own values, as multiple car objects can be made from this constructor
         
@@ -27,6 +29,13 @@ function Car(make,model,year){
 }//End Car
 
 var c = new Car ("Meridian", "Saber GT", "2012");
+//Even though car is a function, when you use the 'new' keyword, it creates an object
 alert(c.getName()); //displays "Meridian Saber GT 2012"
 
 //alert() is a built-in JavaScript function that pops up a small browser dialog box with a message.
+
+var d = new Car("Ford","Focus","2016");
+//creates a new Car object, stored in the variable d
+console.log(d.getName());
+
+
