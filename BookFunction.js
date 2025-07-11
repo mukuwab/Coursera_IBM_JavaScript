0158ff12024e17c207c479b119f2ba0f0dbf432c
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-11 08:41:02 -0500

    Update BookFunction.js

--- BookFunction.js
+++ BookFunction.js
@@ -15,5 +15,23 @@ function Book(title,author,pages){
         isReturned = true;
         isCheckedOut =false;
     }//End return book
-    
+        
+    this.getStatus = function(){
+        if(isReturned == true){
+            console.log("The book is returned");
+        }
+        else if (isCheckedOut == true){
+            console.log("This book has been checked out");
+        }
+        else if(isCheckedOut == false){
+            console.log("This book has not been checked out");
+        }
+        else if(isReturned ==false){
+            console.log("This book has not been returned")
+        }
+    }//End getStatus
+
 }//End function
+
+Book1 = new Book("The Hornet's Nest", "Ella Gray", 23);
+console.
