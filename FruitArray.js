a5d101f5a6d6f0f9fba0bbc26792b8fcb190ba67
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-10 19:39:27 -0500

    Update FruitArray.js

--- FruitArray.js
+++ FruitArray.js
@@ -22,46 +22,57 @@ console.log("\nAfter Adding Fruit\n4th: "+fruits[3]);
 
 //Now remove that last index using the pop method
 fruits.pop();//Will remove the newly added 'cherry' string in the last index
-console.log("\nAfter removing the last index (4th item)\n4th: "+fruits[3]+"\nThe cherry has been removed!");
+console.log("\nAfter removing the last index (4th item)\n4th: "+fruits[3]+"\nThe cherry has been removed!\n");
 
 
 //How many fruits do you have now? 
 //use the 'length' property to find out
 
 let numFruits = fruits.length;
-console.log("\nThere are "+numFruits+" fruits!")
+console.log("There are "+numFruits+" fruits!\n")
 
 //Add more fruits!
 fruits.push("watermelon","blueberry","dragon fruit");
 
 //Now how many?
 numFruits = fruits.length;//To store new values within the numFruits variable
-console.log("\nThere are "+numFruits+" fruits!")
+console.log("There are "+numFruits+" fruits!")
 
-//use a for loop to print out all fruits
-for(let i=0; i<fruits.length; i++){
+//use a functiona and loop to print out all fruits
+function printFruits(){
     
-    console.log(fruits[i])
+    this.length = fruits.length;
+    
+    for(let i=0; i<length; i++){
+    
+   console.log(fruits[i])
     //'i' will increment after every iteration of the loop
     //the new value of i will act as the index
     
-}//End for
+    }//End for
+    console.log("\n");
+}//End print fruits
+
+printFruits();
 
 //use a for loop to remove three fruits from the row
 while (fruits.length > 3) {
   fruits.pop();
 }
 
-console.log("\nAfter removing 3 fruits")
+console.log("After removing 3 fruits:");
+printFruits();
 
-for(let i=0; i<fruits.length; i++){
     
-    console.log(fruits[i])
-    //'i' will increment after every iteration of the loop
-    //the new value of i will act as the index
-    
-}
+
+//Now change the 2nd fruit in the row to an orange!
+fruits[1]="orange";
+console.log("After changing the 2nd fruit to orange:")
+printFruits();
 
 
 
+//notes
+//Do NOT wrap printFruits() in console.log(), because the function itself is already printing.
+//The actual console.log() returns nothing, leaving an undefined value after every loop
 
