9b7d9d853993d7f831936c188d4efcb64844cbdf
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-11 19:32:38 -0500

    Update FilterMethodEx.js

--- FilterMethodEx.js
+++ FilterMethodEx.js
@@ -2,8 +2,7 @@
     //Creates a new array
     //Contains elements that pass specific conditions
     //Extracts specific data
-    
-//Filter books published in the last five years
+
 
 const products = [
     
@@ -15,7 +14,13 @@ const products = [
 ];
 
 function filterProductsByPriceRange(products, minPrice, maxPrice){
-    return products.filter((product) => product.price >= minPrice && product.price <= maxPrice);
+    return products.filter((product) => 
+    
+        product.price >= minPrice && product.price <= maxPrice);
+    //product => product.price >= minPrice && product.price <= maxPrice
+        //Keep this product only if its price is greater than or equal to minPrice AND less than or equal to maxPrice.
+        //Does this product have a price greater than or equal to minPrice
+        //AND is its price less than or equal to maxPrice?
 }
 
 const minPrice = 100;//Minimum price threshold
@@ -24,7 +29,8 @@ const maxPrice = 500; //Maximum price
 const filteredProducts = filterProductsByPriceRange(products, minPrice, maxPrice);
 
 filteredProducts.forEach((product) =>{
+    //filteredProducts is a new array → it only has products where the price is between $100 and $500.
     
-    console.log(`${product.name} is of $${product.price}`);
+    console.log(`${product.name} is $${product.price}`);
 });
     
